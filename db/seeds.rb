12f2f440de7b3bc41425dd2bd2a65e77c8bc765c
oosawa ryouhei <si12005@edu.aomori-u.ac.jp>
2014-09-09 14:48:12 +0900

    画面作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+notes = Note.create([{
+			student_name: '神大樹',
+			student_number: 2,
+			student_grade: 4,
+			student_class: 1,
+			title: 'かじゃ',
+			body: 'こあ',
+			observed_at: '2012-08-28',
+			weather: '晴れ',
+			event_name: '尾駮沼観察',
+			latitude: +Random.rand(0.01)-0.005 ,
+			longitude: +Random.rand(0.01)-0.005,
+}])
