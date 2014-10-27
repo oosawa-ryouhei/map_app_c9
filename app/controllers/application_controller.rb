ff43edd117b6f3dcb81a098e94434a95672f4a93
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-10-27 10:28:57 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,7 @@ class ApplicationController < ActionController::Base
     def check_ip
       if edit_mode?
         unless (controller_name == 'notes' && action_name == 'maintenance')
-=begin
           redirect_to maintenance_url unless request.ip == '202.216.100.21'
-=end
         end
       end
     end
